07b0f12cfc5fb169c9f330b237e78be6d1e5e074
Gerben Neven <gerbyzation@me.com>
2020-03-02 13:42:44 +0000

    use package name on name field

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from distutils.core import setup
 
-setup()
+setup(
+    name='casbin_django_orm_adapter',
+    version='0.1dev',
+    packages=['casbin_django_orm_adapter',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
