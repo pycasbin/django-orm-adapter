dd50b3ec9fb0814186f3174ca4269a4090a46212
Gerben Neven <gerbyzation@me.com>
2020-03-02 13:22:51 +0000

    create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Casbin Django ORM Adapter',
+    version='0.1dev',
+    packages=['casbin_django_orm_adaptor',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
