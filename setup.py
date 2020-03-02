7189a222e08b9146d6b9f0d99bb7065d377f08cf
Gerben Neven <gerbyzation@me.com>
2020-03-02 13:24:18 +0000

    fix typo in package location

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Casbin Django ORM Adapter',
     version='0.1dev',
-    packages=['casbin_django_orm_adaptor',],
+    packages=['casbin_django_orm_adapter',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
