b3e6577d50886957e573fbd88995501361efbd56
Gerben Neven <gerbyzation@me.com>
2020-05-17 17:05:08 +0200

    remove test django project, update test settings

--- authorization/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-from casbin_adapter.enforcer import enforcer
-# Create your views here.
--- tests/settings.py
+++ tests/settings.py
@@ -11,7 +11,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "casbin_adapter",
+    "casbin_adapter.apps.CasbinAdapterConfig",
     "tests",
 ]
 
