d94e7dba8fe4204e12a2752fd7cf29b7686e33ad
Gerben Neven <gerbyzation@me.com>
2020-05-22 12:29:07 +0200

    add logging, fix small issues

--- casbin_adapter/utils.py
+++ casbin_adapter/utils.py
@@ -4,7 +4,7 @@ def import_class(name):
     """Import class from string
     e.g. `package.module.ClassToImport` returns the `ClasToImport` class"""
     components = name.split('.')
-    module_name = components[:-1].join('.')
+    module_name = '.'.join(components[:-1])
     class_name = components[-1]
     module = importlib.import_module(module_name)
     class_ = getattr(module, class_name)
